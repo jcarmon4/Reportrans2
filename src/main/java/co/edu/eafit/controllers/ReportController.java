45a9398d47dbd97e9f455935a51410f46cb356d8
unknown <alejocram@gmail.com>
2018-09-18 13:04:07 -0500

    Se crea el metodo para permitir crear un nuevo reporte

--- src/main/java/co/edu/eafit/controllers/ReportController.java
+++ src/main/java/co/edu/eafit/controllers/ReportController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +36,13 @@ public class ReportController {
 			reportList.add(report);
 			return gson.toJson(reportList);
 		}
+	}
+	
+	@PostMapping(path="/report", consumes="application/json", produces="application/json")
+	public String createReport(@RequestBody String json) {
+		Gson gson = new Gson();
+		Report report = gson.fromJson(json, Report.class);
+		reportMap.put(report.getCarPlate(), report);
+		return gson.toJson(report);
 	}
 }
