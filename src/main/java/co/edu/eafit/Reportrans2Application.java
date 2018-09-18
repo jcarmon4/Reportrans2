219ba7fca57b60c6fd8aedadc0c31792766d8867
unknown <alejocram@gmail.com>
2018-09-17 20:30:09 -0500

    Se crea el proyecto gradle

---
+++ src/main/java/co/edu/eafit/Reportrans2Application.java
@@ -0,0 +1,12 @@
+package co.edu.eafit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reportrans2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reportrans2Application.class, args);
+	}
+}
