3bb40acfd1779c89ed2ebd146a550673e799a88f
unknown <alejocram@gmail.com>
2018-09-17 20:46:07 -0500

    Se crea la entidad Report

---
+++ src/main/java/co/edu/eafit/models/Report.java
@@ -0,0 +1,65 @@
+package co.edu.eafit.models;
+
+public class Report {
+	private Integer id;
+	private String carPlate;
+	private String type;
+	private Integer score;
+	private String comment;
+
+	public Report() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Report(Integer id, String carPlate, String type, Integer score, String comment) {
+		super();
+		this.id = id;
+		this.carPlate = carPlate;
+		this.type = type;
+		this.score = score;
+		this.comment = comment;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCarPlate() {
+		return carPlate;
+	}
+
+	public void setCarPlate(String carPlate) {
+		this.carPlate = carPlate;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Integer getScore() {
+		return score;
+	}
+
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
+
+}
